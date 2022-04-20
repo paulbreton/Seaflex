74953e04105ae6d779d87932546954310bee1664
Malo <malo.louboutin@yahoo.fr>
2022-04-20 15:32:55 +0200

    add migration for ue,module and ueContent

---
+++ Seaflex/database/migrations/2022_04_20_125728_create_ue_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ue', function (Blueprint $table) {
+            $table->string('code');
+            $table->enum('modalite',['Majeur','Mineur']);
+            $table->string('libellé');
+            $table->boolean('impaire');
+            $table->boolean('paire');
+            $table->string('capacité');
+            $table->enum('ects',[9,21]);
+            $table->primary('code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ue');
+    }
+};
