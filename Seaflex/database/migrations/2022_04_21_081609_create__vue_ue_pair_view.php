04d5346b219d8703bcd93953bc0016a460d0554a
MaloLouboutin <mloubout@enssat.fr>
2022-04-21 12:34:35 +0200

    add views

---
+++ Seaflex/database/migrations/2022_04_21_081609_create__vue_ue_pair_view.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement($this->dropView());
+
+        DB::statement($this->createView());
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement($this->dropView());
+    }
+
+    private function createView() : string {
+        return <<<SQL
+            create view vueUePaire as
+            select * from ue where paire = true;
+        SQL;
+    }
+
+    private function dropView() : string {
+        return <<<SQL
+            DROP VIEW IF EXISTS vueUePaire;
+        SQL;
+    }
+};
