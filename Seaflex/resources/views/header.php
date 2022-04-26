5c605487d1bd9644ff60481e5b7218576261e594
paulbreton <88530677+paulbreton@users.noreply.github.com>
2022-04-26 21:35:20 +0200

    Update header.php

--- Seaflex/resources/views/header.php
+++ Seaflex/resources/views/header.php
@@ -30,7 +30,7 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="<?php echo url(env('APP_URL').'/') ?>">SchoFlex</a>
+            <a class="navbar-brand" href="<?php echo url(env('APP_URL').'/') ?>">ScoFlex</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
